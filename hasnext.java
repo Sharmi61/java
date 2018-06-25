52cc7349106d8031755b017151df1968ce8a6361
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-06-25 15:36:52 +0530

    Add files via upload

---
+++ hasnext.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class hasnext {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+        int i=1;
+        for(i=1;sc.hasNext()==true;i++){
+            System.out.println(i+" "+sc.nextLine());
+		
+	}
+
+}}
