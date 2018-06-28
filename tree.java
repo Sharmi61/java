80d7013fe889152670464cfcffc9e3c9ae983247
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-06-28 14:42:04 +0530

    Add files via upload

---
+++ tree.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
+
+
+
+	public class tree {
+		public static void main(String args[]) {
+			int s = 252500, t = 0, c = 0;
+			ArrayList<Integer> al = new ArrayList<>();
+			TreeSet<Integer> al1 = new TreeSet<>();
+			int i = 0;
+			while (s > 0) {
+				t = s % 10;
+				al.add(t);
+				s = s / 10;
+			}
+			for (i = 0; i < al.size(); i++) {
+			//	c = Collections.frequency(al, i);
+				//if (c > 1) {
+					al1.add(al.get(i));
+				//}
+			}
+			System.out.println("max=" + al1);
+		}
+	}
+
+
