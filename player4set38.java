7f8a60516d458a4a8254f2961e03151a625fa6ea
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-08-30 11:38:22 +0530

    Update player4set38.java

--- player4set38.java
+++ player4set38.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class player4set38 {
 public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
-	int n=sc.nextInt();
+	int n=sc.nextInt();int a=0;
 	for(int i=2;i<=n;i++)
 	{
 		if(n%i==0)
-		{if(i%2==0)
-			System.out.print(i+" ");
+		{if(i%2==0){if(a!=0)
+		{System.out.print(" "+i);a=a+1;}
+			   else
+			   System.out.prinln(i);}
 		}
 	}
 }
