435702c58c1d26a42de89ad589e04f70c066e3c0
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-12-18 01:53:50 -0800

    Add files via upload

---
+++ playerset43.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class playerset43{
+public static void main(String[] args)
+{Scanner sc=new Scanner(System.in);
+String s1=sc.next();
+String s2=sc.next();
+/*int a=s1.lastIndexOf(s2);//tells the rightmost occurance
+if(a>=0)
+System.out.println(a);
+else
+System.out.println(a);
+int b=s1.lastIndexOf(s2);//tells the first occurance
+if(b>=0)
+System.out.println(b);
+else
+System.out.println(b);*/
+if(s1.contains(s1))
+System.out.println("yes");
+else
+System.out.println("no");
+}}
